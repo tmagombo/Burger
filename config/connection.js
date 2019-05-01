0599fa109720e0cbc4e8a4f69c150046f23334e3
Tristan <magombotristan@gmail.com>
2019-05-01 13:14:05 -0700

    Added the connection, handlebars, orm, and controller

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "tristan9014219",
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("Burger/public"));
+
+
+require("./app/routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
