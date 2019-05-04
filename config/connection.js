38c957a8a008c30c73639b76b6237c79cc9f1ace
Tristan <magombotristan@gmail.com>
2019-05-04 11:44:08 -0700

    Fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "tristan9014219",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
